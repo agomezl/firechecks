19acf92549d809d8eaf14baa320cc0257faa7832
Alejandro Gomez-Londono <alegomez544@gmail.com>
2018-02-09 20:21:13 +0100

    Add basic http server

--- server/server.py
+++ server/server.py
@@ -1,12 +1,35 @@
-import SimpleHTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
-from parseurl import parseurl, parse_qs
+from urlparse import urlparse, parse_qs
 
-PORT = 8000
+PORT = 8082
+ENPOINT = ['/lab92']
 
 class fireHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        query = parse_qs(urlparse(self.path).query)
-        _cb   = query['cb'][0]
-        _zip  = query['zip'][0]
+        request = urlparse(self.path)
+        query   = parse_qs(request.query)
+        path    = request.path
+        _cb     = query['cb'][0]
+        _zip    = query['zip'][0]
+
+        if path in ENPOINT:
+            self.send_response(200, 'Ok')
+        else:
+            self.send_error(404, 'Path is not defined')
+        self.end_headers()
+        return
+
+def run():
+  print('http server is starting...')
+
+  #ip and port of server
+  #by default http server port is 80
+  server_address = ('localhost', PORT)
+  httpd = HTTPServer(server_address, fireHandler)
+  print('http server is running on port %s...' % PORT)
+  httpd.serve_forever()
+
+if __name__ == '__main__':
+  run()
