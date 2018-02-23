43971fc0cb9fc4e848f4f13425d0dc680a32ff8b
Alejandro Gomez-Londono <alegomez544@gmail.com>
2018-02-23 20:04:48 +0100

    Improve rabbit.sh script

--- rabbit.sh
+++ rabbit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+MQ_HOST=${MQ_HOST:-"rabbit"}
+COURSE_NAME=${COURSE_NAME:-"testLab"}
+
 # Some setup
 docker kill rabbit &> /dev/null
 docker rm rabbit &> /dev/null
@@ -7,24 +10,16 @@ mkdir -p ~/.rabbit-docker/data &> /dev/null
 chmod -R 777 ~/.rabbit-docker/ &> /dev/null
 
 # Create network
-docker network create testLab
+docker network create ${COURSE_NAME}
 
 # Launch the docker
 echo "Building and starting the container..."
-
-case "$OSTYPE" in
-    #solaris*) echo "SOLARIS" ;;
-    darwin*)
-        ID=$(docker run -d --hostname rabbit --name rabbit rabbitmq:3)
-        ;;
-    linux*)
-        ID=$(docker run -d -p 5672:5672 --hostname rabbit --net testLab --name rabbit -v ~/.rabbit-docker/data:/var/lib/rabbitmq rabbitmq:3)
-        ;;
-    #bsd*)     echo "BSD" ;;
-    #msys*)    echo "WINDOWS" ;;
-    *)        echo "Unhandled OS: $OSTYPE" ;;
-esac
+ID=$(docker run -d -p 5672:5672 \
+            --hostname ${MQ_HOST} \
+            --net ${COURSE_NAME} \
+            --name ${MQ_HOST} \
+            -v ~/.rabbit-docker/data:/var/lib/rabbitmq \
+            rabbitmq:3)
 
 echo "Container ID: $ID"
-
 echo "Can now be started and stopped with \`docker [start|stop] rabbit\`"
