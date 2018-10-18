6845be8bd8ec63c7ce5528964a7fac297b9cac95
Alejandro Gomez-Londono <alegomez544@gmail.com>
2018-10-18 21:28:20 +0200

    Add startup script

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export COURSE_NAME=${COURSE_NAME:-"testLab"}
+export LAB_NUMBER=${LAB_NUMBER:-1}
+export MQ_HOST=${MQ_HOST:-"rabbit"}
+
+
+if ! docker container ls | grep -q ${MQ_HOST}
+then
+    ./rabbit.sh
+    sleep 5
+fi
+
+
+if ! docker container ls | grep -q "submissionserver_${COURSE_NAME}"
+then
+    ./submissionserver.sh
+    sleep 5
+fi
+
+if ! docker container ls | grep -q "testserver_${COURSE_NAME}"
+then
+    ./testserver.sh
+fi
